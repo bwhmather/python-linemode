68c35fbe15aafd07bb67d0e279222efa3fb19828
Ben Mather <bwhmather@bwhmather.com>
2014-08-28 12:09:48 +0100

    basic test for star printer driver

--- nm_printing/tests/__init__.py
+++ nm_printing/tests/__init__.py
@@ -1,9 +1,11 @@
 import unittest
 
 from nm_printing.tests.test_line_mode_renderer import TestLineModeRenderer
+from nm_printing.tests.test_star import TestStarPrinter
 
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite((
     loader.loadTestsFromTestCase(TestLineModeRenderer),
+    loader.loadTestsFromTestCase(TestStarPrinter),
 ))

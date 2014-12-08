b58de098ea7c3a94c305f9614827639a95622758
Ben Mather <bwhmather@bwhmather.com>
2014-12-08 17:28:47 +0000

    command list renderer

--- linemode/tests/__init__.py
+++ linemode/tests/__init__.py
@@ -1,6 +1,7 @@
 import unittest
 
 from linemode.tests.test_xml_renderer import TestXMLRenderer
+from linemode.tests.test_command_list_renderer import TestCommandListRenderer
 from linemode.tests.test_star import TestStarPrinter
 from linemode.tests.test_spooler import TestPrintSpooler
 
@@ -8,6 +9,7 @@ from linemode.tests.test_spooler import TestPrintSpooler
 loader = unittest.TestLoader()
 suite = unittest.TestSuite((
     loader.loadTestsFromTestCase(TestXMLRenderer),
+    loader.loadTestsFromTestCase(TestCommandListRenderer),
     loader.loadTestsFromTestCase(TestStarPrinter),
     loader.loadTestsFromTestCase(TestPrintSpooler),
 ))

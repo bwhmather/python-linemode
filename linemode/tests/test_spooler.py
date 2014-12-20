c2b8844f27b197766a08c6d3dd62843abc3d6eeb
Ben Mather <bwhmather@bwhmather.com>
2014-12-20 11:21:34 +0000

    add test for submitting jobs to print spooler

--- linemode/tests/test_spooler.py
+++ linemode/tests/test_spooler.py
@@ -10,3 +10,43 @@ class TestPrintSpooler(unittest.TestCase):
             pass
         spooler = PrintSpooler(DummyPrinter())
         spooler.shutdown()
+
+    def test_submit_job(self):
+        testcase = self
+        dummy_commands = object()
+        compiled = False
+
+        dummy_program = object()
+        executed = False
+
+        class DummyPrinter(Printer):
+            def compile(self, commands):
+                nonlocal testcase
+                nonlocal dummy_commands
+                nonlocal compiled
+                nonlocal dummy_program
+
+                testcase.assertIs(commands, dummy_commands)
+                testcase.assertFalse(compiled)
+
+                compiled = True
+
+                return dummy_program
+
+            def execute(self, program):
+                nonlocal testcase
+                nonlocal dummy_program
+                nonlocal executed
+
+                testcase.assertIs(program, dummy_program)
+                testcase.assertFalse(executed)
+
+                executed = True
+
+        spooler = PrintSpooler(DummyPrinter())
+        spooler.submit(dummy_commands)
+
+        spooler.shutdown()
+
+        self.assertTrue(compiled)
+        self.assertTrue(executed)

ba5cbb0052954c0e8e7970476c5252fe457592c2
Ben Mather <bwhmather@bwhmather.com>
2015-01-22 17:28:46 +0000

    first pass at implementing command list compiler

--- linemode/drivers/command_list.py
+++ linemode/drivers/command_list.py
@@ -3,9 +3,25 @@ from urllib.parse import urlparse
 from linemode.base import Printer
 
 
+def _compile_command(command):
+    if isinstance(command, str):
+        command_name, args = command, []
+    else:
+        command_name, *args = command
+
+    if len(args):
+        return (
+            command_name + ": " +
+            ", ".join(repr(arg) for arg in args)
+        ).encode('utf-8')
+    else:
+        return command_name.encode('utf-8')
+
+
 def compile(commands):
-    # TODO
-    raise NotImplementedError()
+    return b'\n'.join(
+        _compile_command(command) for command in commands
+    )
 
 
 class CommandListPrinter(Printer):

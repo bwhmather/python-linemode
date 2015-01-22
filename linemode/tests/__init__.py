e6eda7c86f44d44be90e7be761554520377e7705
Ben Mather <bwhmather@bwhmather.com>
2015-01-22 17:26:09 +0000

    fix command_list_renderer tests not being run

--- linemode/tests/__init__.py
+++ linemode/tests/__init__.py
@@ -12,7 +12,8 @@ loader = unittest.TestLoader()
 suite = unittest.TestSuite((
     loader.loadTestsFromModule(test_loader),
     loader.loadTestsFromModule(test_xml_renderer),
-    loader.loadTestsFromModule(test_command_list_printer),
+    loader.loadTestsFromModule(test_command_list_renderer),
     loader.loadTestsFromModule(test_star),
+    loader.loadTestsFromModule(test_command_list_printer),
     loader.loadTestsFromModule(test_spooler),
 ))

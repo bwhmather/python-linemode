2b3940c150a2078a914b01b45e3042b0f788e4fa
Ben Mather <bwhmather@bwhmather.com>
2015-03-16 15:06:53 +0000

    bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras_require = {
 setup(
     name='linemode',
     url='github.com/bwhmather/python-linemode',
-    version='0.3.0',
+    version='0.4.0',
     author='Ben Mather',
     author_email='bwhmather@bwhmather.com',
     maintainer='',
